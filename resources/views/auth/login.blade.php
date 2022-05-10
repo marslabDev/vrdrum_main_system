ff8b0fc29dadf3bd49be600139763c2df38de5e7
boonjun <boonjun@marslab.com.my>
2022-05-10 17:41:24 +0800

    auto fill on local env

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,9 +9,9 @@
                 <p class="text-muted">{{ trans('global.login') }}</p>
 
                 @if(session('message'))
-                    <div class="alert alert-info" role="alert">
-                        {{ session('message') }}
-                    </div>
+                <div class="alert alert-info" role="alert">
+                    {{ session('message') }}
+                </div>
                 @endif
 
                 <form method="POST" action="{{ route('login') }}">
@@ -24,12 +24,15 @@
                             </span>
                         </div>
 
-                        <input id="email" name="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required autocomplete="email" autofocus placeholder="{{ trans('global.login_email') }}" value="{{ old('email', null) }}">
+                        <input id="email" name="email" type="text"
+                            class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required
+                            autocomplete="email" autofocus placeholder="{{ trans('global.login_email') }}"
+                            value="{{  old('email',  env('APP_ENV') == 'local' ? 'admin@admin.com' : null) }}">
 
                         @if($errors->has('email'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('email') }}
-                            </div>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('email') }}
+                        </div>
                         @endif
                     </div>
 
@@ -38,18 +41,22 @@
                             <span class="input-group-text"><i class="fa fa-lock"></i></span>
                         </div>
 
-                        <input id="password" name="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required placeholder="{{ trans('global.login_password') }}">
+                        <input id="password" name="password" type="password"
+                            class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required
+                            value="{{ env('APP_ENV') == 'local' ? 'password' : null }}"
+                            placeholder="{{ trans('global.login_password') }}">
 
                         @if($errors->has('password'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('password') }}
-                            </div>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('password') }}
+                        </div>
                         @endif
                     </div>
 
                     <div class="input-group mb-4">
                         <div class="form-check checkbox">
-                            <input class="form-check-input" name="remember" type="checkbox" id="remember" style="vertical-align: middle;" />
+                            <input class="form-check-input" name="remember" type="checkbox" id="remember"
+                                style="vertical-align: middle;" />
                             <label class="form-check-label" for="remember" style="vertical-align: middle;">
                                 {{ trans('global.remember_me') }}
                             </label>
@@ -64,9 +71,9 @@
                         </div>
                         <div class="col-6 text-right">
                             @if(Route::has('password.request'))
-                                <a class="btn btn-link px-0" href="{{ route('password.request') }}">
-                                    {{ trans('global.forgot_password') }}
-                                </a><br>
+                            <a class="btn btn-link px-0" href="{{ route('password.request') }}">
+                                {{ trans('global.forgot_password') }}
+                            </a><br>
                             @endif
 
                         </div>
