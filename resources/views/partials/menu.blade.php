bbb94a077a68d0b2217feb21c96c6daf5e80daa9
LimWeiHong <weihong902966@gmail.com>
2022-05-16 09:17:22 +0800

    remove double audit-log in menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -335,16 +335,6 @@
             </a>
         </li>
         @endcan
-        @can('audit_log_access')
-            <li class="c-sidebar-nav-item">
-                <a href="{{ route("admin.audit-logs.index") }}" class="c-sidebar-nav-link {{ request()->is("admin/audit-logs") || request()->is("admin/audit-logs/*") ? "c-active" : "" }}">
-                    <i class="fa-fw fas fa-file-alt c-sidebar-nav-icon">
-
-                    </i>
-                    {{ trans('cruds.auditLog.title') }}
-                </a>
-            </li>
-        @endcan
         @can('branch_management_access')
             <li class="c-sidebar-nav-dropdown {{ request()->is("admin/branches*") ? "c-show" : "" }}">
                 <a class="c-sidebar-nav-dropdown-toggle" href="#">
